09c0c8e242443e28764dc5f10723f00975683d35
StiliyanIvanov02 <stilian02ivanov@gmail.com>
2021-08-30 21:10:43 +0300

    002 30.08

---
+++ Searching And Sorting Algorithms/Sort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching_And_Sorting_Algorithms
+{
+    public static class Sort
+    {
+        public static int[] BubbleSort(int[] array)
+        {
+            return array;
+        }
+    }
+}
