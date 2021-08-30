5ad9665971f29eaf21db71a8ea9555342c91cb6c
StiliyanIvanov02 <stilian02ivanov@gmail.com>
2021-08-30 21:25:07 +0300

    003 30.08

--- Searching And Sorting Algorithms/Program.cs
+++ Searching And Sorting Algorithms/Program.cs
@@ -10,10 +10,9 @@ namespace Searching_And_Sorting_Algorithms
     {
         static void Main(string[] args)
         {
-            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-            int number = int.Parse(Console.ReadLine());
-            int result = Search.ExponentialSearch(array, number);
-            Console.WriteLine(result);
+            int[] array = { 8, 1, 2, 3, 4, 7, 6, 20, 15, 0, 19, 11, 12, 13, 14, 10, 17, 16, 18, 9, 5};
+            int[] sortedArray = Sort.BubbleSort(array);
+            Console.WriteLine(String.Join(", ", sortedArray));
         }
     }
 }
--- Searching And Sorting Algorithms/Sort.cs
+++ Searching And Sorting Algorithms/Sort.cs
@@ -8,8 +8,39 @@ namespace Searching_And_Sorting_Algorithms
 {
     public static class Sort
     {
-        public static int[] BubbleSort(int[] array)
+        public static int[] BubbleSort(int[] array, bool asc = true)
         {
+            int length = array.Length;
+            if (asc)
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    for (int j = 0; j < length - i - 1; j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            int swap = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = swap;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    for (int j = 0; j < length - i - 1; j++)
+                    {
+                        if (array[j] < array[j + 1])
+                        {
+                            int swap = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = swap;
+                        }
+                    }
+                }
+            }
             return array;
         }
     }
